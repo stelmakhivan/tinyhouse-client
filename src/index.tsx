b784b043acf1238c833990b4187850e90b0acfa4
stelmakhivan <stelmakh.developer@gmail.com>
2020-10-04 19:44:41 +0300

    Added Login page

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,20 @@ import { render } from 'react-dom'
 import ApolloClient from 'apollo-boost'
 import { ApolloProvider } from 'react-apollo'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { Layout } from 'antd'
 import * as serviceWorker from './serviceWorker'
 
 import './styles/index.css'
 
-import { Listings, Home, Host, Listing, NotFound, User } from './sections'
+import {
+  Listings,
+  Home,
+  Host,
+  Listing,
+  NotFound,
+  User,
+  Login,
+} from './sections'
 
 const client = new ApolloClient({
   uri: '/api',
@@ -16,14 +25,17 @@ const client = new ApolloClient({
 const App = () => {
   return (
     <Router>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/host" component={Host} />
-        <Route exact path="/listings/:location?" component={Listings} />
-        <Route exact path="/listing/:id" component={Listing} />
-        <Route exact path="/user/:id" component={User} />
-        <Route component={NotFound} />
-      </Switch>
+      <Layout id="app">
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/host" component={Host} />
+          <Route exact path="/listings/:location?" component={Listings} />
+          <Route exact path="/listing/:id" component={Listing} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/user/:id" component={User} />
+          <Route component={NotFound} />
+        </Switch>
+      </Layout>
     </Router>
   )
 }
