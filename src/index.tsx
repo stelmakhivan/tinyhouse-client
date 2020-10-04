236eee4d9a9062872d916f839dddeb2fbab54fb7
stelmakhivan <stelmakh.developer@gmail.com>
2020-10-04 15:15:46 +0300

    Added react-router-dom with basic routes

--- src/index.tsx
+++ src/index.tsx
@@ -2,19 +2,35 @@ import React from 'react'
 import { render } from 'react-dom'
 import ApolloClient from 'apollo-boost'
 import { ApolloProvider } from 'react-apollo'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import * as serviceWorker from './serviceWorker'
 
 import './styles/index.css'
 
-import { Listings } from './sections'
+import { Listings, Home, Host, Listing, NotFound, User } from './sections'
 
 const client = new ApolloClient({
   uri: '/api',
 })
 
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/host" component={Host} />
+        <Route exact path="/listings/:location?" component={Listings} />
+        <Route exact path="/listing/:id" component={Listing} />
+        <Route exact path="/user/:id" component={User} />
+        <Route component={NotFound} />
+      </Switch>
+    </Router>
+  )
+}
+
 render(
   <ApolloProvider client={client}>
-    <Listings title="TinyHouse Listings" />
+    <App />
   </ApolloProvider>,
   document.getElementById('root')
 )
